7912f374cc424ab2c4d055e8a61b276ceb42f374
Ashik Zinnat Khan <ashrik@gmail.com>
2015-04-21 03:34:21 +0600

    - Fixed rare playability bug caused by missing year field in meta data - Added new version of the addon

--- plugin.video.mvl.one/plugin.video.mvl.one/default.py
+++ plugin.video.mvl.one/plugin.video.mvl.one/default.py
@@ -1428,6 +1428,7 @@ def create_meta(video_type, title, year, thumb, sub_cat=None, imdb_id=''):
         year_int = year
     except:
         year = 0
+        year_int = year
     year = str(year)
     meta = {'title': title, 'year': year, 'imdb_id': '', 'overlay': '', 'duration': '', 'playcount': '' }
     try:
