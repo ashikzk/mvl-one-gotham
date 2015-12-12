1790d2015d555b47aeabdd757ece6e1592560e59
Ashik Zinnat Khan <ashrik@gmail.com>
2015-12-13 02:11:12 +0600

    - Added new premium list ordering - Added latest url resolver

--- plugin.video.mvl.one/plugin.video.mvl.one/default.py
+++ plugin.video.mvl.one/plugin.video.mvl.one/default.py
@@ -1081,7 +1081,8 @@ def get_videos(id, thumbnail, trailer, parent_id, series_name):
                           'replace_context_menu': True
                       }]
 
-            src_list = ['movreel', 'firedrive', 'putlocker', 'mightyupload', 'promptfile', 'novamov', 'thefile', 'bestream', 'nowvideo', 'gorillavid']
+            src_list = ['gorillavid', 'bestreams', 'thevideo', 'stagevu', 'vidzi', 'vidto', 'vidbull', 'nowvideo', 'movshare', 'videoweed', 'novamov', 'movpod', 'mightyupload']
+            # ['movreel', 'firedrive', 'putlocker', 'mightyupload', 'promptfile', 'novamov', 'thefile', 'bestream', 'nowvideo', 'gorillavid']
             # ['movreel', 'firedrive', 'putlocker', 'mightyupload', 'promptfile', 'novamov', 'realvid', 'streamin', 'vidzi', 'vidbull', 'thefile', 'bestream', 'nowvideo', 'gorillavid']
             # ['movreel', 'mightyupload', 'promptfile', 'firedrive', 'putlocker', 'novamov', 'nowvideo', 'gorillavid']
 
